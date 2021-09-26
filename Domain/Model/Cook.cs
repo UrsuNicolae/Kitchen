853453b2f2c58f7b58e455b923b05b85622655da
Nicolae Ursu <nicolae.ursu@codefactorygroup.com>
2021-09-26 21:45:04 +0300

    Initialization

---
+++ Domain/Model/Cook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public sealed class Cook
+    {
+        public Guid Id { get; set; }
+
+        public Order PrepareOrder(Order order)
+        {
+            return order;
+        }
+    }
+}
