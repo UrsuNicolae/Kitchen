7bb85f1117981785d18eafeb23a50a4a589cab75
Nicolae Ursu <nicolae.ursu@codefactorygroup.com>
2021-09-27 22:30:56 +0300

    Add http client

--- Domain/Model/Cook.cs
+++ Domain/Model/Cook.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Domain.Model
 {
     public sealed class Cook
     {
-        public Guid Id { get; set; }
+        public Cook()
+        {
+            Id = new Guid();
+            IsAvailable = true;
+        }
+
+        public Guid Id { get;}
+
+        public bool IsAvailable { get; set; }
 
         public Order PrepareOrder(Order order)
         {
+            IsAvailable = false;
+            Thread.Sleep(100);
+            IsAvailable = true;
             return order;
         }
     }
